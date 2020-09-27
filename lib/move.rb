48a80188d602b0a1380aa505775e505f776ce22e
proactive-researcher-8324 <marymichelle1118@gmail.com>
2020-09-27 02:05:01 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -45,3 +45,16 @@ valid_move?(board, index)
 position_taken?(board, index)
 input_to_index(user_input)
 move(board, index, first_player = "X")
+
+def turn_count(board)
+   counter = 0
+   board.each do |spaces|
+      if spaces == "X" || spaces == "O"
+         counter += 1
+      end
+   end
+   counter
+end
+def current_player(board)
+   turn_count(board) % 2 == 0 ? "X" : "O"
+end
