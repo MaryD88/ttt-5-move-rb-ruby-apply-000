93f6bca386724f7bfb789ebaa19a77d83cc40229
proactive-researcher-8324 <marymichelle1118@gmail.com>
2020-09-27 02:09:38 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -22,8 +22,4 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, index, player)
-  board[index] = player
-end
-
 puts board = [' ', ' ', 'X', ' ', ' ', ' ', ' ', ' ', ' ']
