cee917cdd8d9e275152949f7d84fb6d95587b711
proactive-researcher-8324 <marymichelle1118@gmail.com>
2020-09-27 02:03:45 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -26,4 +26,10 @@ def move(board, index, player)
   board[index] = player
 end
 
-
+def position_taken?(board, index)
+   if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
+      return false 
+   else
+      return true
+   end
+end
